6f0a4dc42b06694d6ecbf64faa485ec93cb25151
Tyler McQueen <tmcqueen@e-gineering.com>
2020-01-29 17:57:40 -0500

    initing

---
+++ src/main/java/TicTacToePage.java
@@ -0,0 +1,35 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.not;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TicTacToePage {
+
+    private ElementsCollection squares = $$(".square");
+    private SelenideElement restart = $(".restart");
+
+
+    public TicTacToePage() {
+        open("https://playtictactoe.org/");
+    }
+
+    public void clickASquare() {
+        for (SelenideElement square : squares) {
+            if (!square.$(".x").exists() && !square.$(".o").exists()) {
+                square.click();
+                return;
+            }
+        }
+        throw new RuntimeException("No Squares to click");
+    }
+
+    public boolean isGameOver(){
+        try {
+            restart.waitWhile(not(Condition.visible), 500);
+        } catch(Throwable ignore){}
+        return restart.isDisplayed();
+    }
+}
---
+++ src/test/java/ThisIsATest.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Config;
+import com.codeborne.selenide.Configuration;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ThisIsATest {
+
+    @BeforeTest
+    public void setup(){
+        Configuration.browser="chrome";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 1000;
+    }
+
+    @Test
+    public void testMethod() throws InterruptedException {
+        TicTacToePage ticTacToePage = new TicTacToePage();
+
+        while(!ticTacToePage.isGameOver()){
+            ticTacToePage.clickASquare();
+        }
+
+        Assert.assertTrue(ticTacToePage.isGameOver());
+
+
+    }
+}
